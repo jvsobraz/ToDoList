175445b1265d63a89fe287cf43fe90b27313230c
jvsobraz <jvsobraz@hotmail.com>
2023-09-11 17:43:03 -0300

    Utilizandos o conceito de props e estilização dos elementos criados

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react'
 
+import Todo from "./components/Todo";
+import TodoForm from './components/TodoForm';
+
+import "./App.css";
+
 function App() {
   const [todos, setTodos] = useState([
     {
@@ -27,18 +32,10 @@ function App() {
       <h1>Lista de Tarefas</h1>
       <div className='todo-list'>
         {todos.map((todo) => (
-          <div className='todo'>
-            <div className='content'>
-              <p>{todo.text}</p>
-              <p className='category'>({todo.category})</p>
-            </div>
-            <div>
-              <button>Completar</button>
-              <button>x</button>
-            </div>
-          </div>
+          <Todo key={todo.id} todo={todo} />
         ))}
       </div>
+      <TodoForm />
     </div>
   );
 }
